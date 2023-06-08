d09d70665cc0ef39e120191fd5b6013e7281d165
Underskinua <zovadim@gmail.com>
2023-06-09 02:41:04 +0300

    add-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,48 +1,29 @@
+// 02-lightbox.js
 import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
-const createGalleryItem = ({ preview, original, description }) => {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
+// Generate gallery items markup
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+         <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+           <div class="sl-caption" style="opacity: 0; transition-delay: 250ms; color: white;">${description}</div>
+           </a>
+       </li>`
+  )
+  .join('');
 
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.href = original;
+// Add gallery items to the list
+gallery.innerHTML = galleryItemsMarkup;
 
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.src = preview;
-  galleryImage.alt = description;
+// Initialize SimpleLightbox
+new SimpleLightbox('.gallery a', {
+  /* options */
+});
 
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
 
-  return galleryItem;
-};
-
-const galleryMarkup = galleryItems.map(createGalleryItem);
-gallery.append(...galleryMarkup);
-
-// Initialize SimpleLightbox after adding gallery items
-const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-  animationSpeed: 250,
-  overlay: true,
-  overlayOpacity: 0.9,
-  captionClass: 'custom-caption',
-  elementsSelector: '.gallery__item',
-  docClose: false,
-  onOpen: () => {
-    document.querySelector('.simple-lightbox').style.display = 'flex';
-    document.querySelector('.simple-lightbox').style.alignItems = 'center';
-    document.querySelector('.simple-lightbox').style.justifyContent = 'center';
-    document.querySelector('.simple-lightbox').style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-    document.querySelector('.simple-lightbox__image').style.transform = 'none';
-  },
-  onClose: () => {
-    document.querySelector('.simple-lightbox').style.display = 'none';
-  },
-  });
