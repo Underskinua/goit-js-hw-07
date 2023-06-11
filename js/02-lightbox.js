0e47c3aabdf18496a5bf01c86826fcd9881b8db8
Underskinua <zovadim@gmail.com>
2023-06-11 16:39:45 +0300

    fixed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,20 +10,23 @@ const galleryItemsMarkup = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<li class="gallery__item">
-         <a class="gallery__link" href="${original}">
-           <img class="gallery__image" src="${preview}" alt="${description}" />
-           <div class="sl-caption" style="opacity: 0; transition-delay: 250ms; color: white;">${description}</div>
-           </a>
-       </li>`
-  )
-  .join('');
+      <a class="gallery__link" href="${original}" data-caption="${description}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      <div class="sl-caption">${description}</div>
+    </a>
+  </li>`
+)
+.join('');
 
 // Add gallery items to the list
 gallery.innerHTML = galleryItemsMarkup;
 
 // Initialize SimpleLightbox
 new SimpleLightbox('.gallery a', {
-  /* options */
+  captions: true,
+  captionSelector: 'self', // Отримати підпис з елементу A-Tag
+  captionType: 'data', // Отримати підпис з атрибута 'data-caption'
+  captionsData: 'caption', // Використовувати значення атрибута 'data-caption' як підпис
+  captionDelay: 250, // Затримка перед появою підпису (в мілісекундах)
 });
-
 
